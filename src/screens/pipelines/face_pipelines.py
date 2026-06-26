7bd539a7629cba6816d159843404171f08eb494e
Kiran Swamy K Aradyamath <kiranswamykaradyamath@gmail.com>
2026-06-26 23:49:58 +0530

    Delete src directory

--- src/screens/pipelines/face_pipelines.py
+++
@@ -1,7 +0,0 @@
-from src.pipelines.face_pipelines import (
-    get_face_embeddings,
-    get_trained_model,
-    predict_attendance,
-    predict_attendence,
-    train_classifier,
-)
--- src/screens/pipelines/voice_pipelines.py
+++
@@ -1,6 +0,0 @@
-from src.pipelines.voice_pipelines import (
-    get_voice_embedding,
-    identify_speaker,
-    load_voice_encoder,
-    process_bulk_audio,
-)
